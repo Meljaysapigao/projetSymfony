0563b1c3383eed4bcd598bfbb303377313baff9b
Meljaysapigao <mel.sapi@laposte.net>
2024-03-17 20:42:52 +0100

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+// class HomeController extends AbstractController
+// {
+//     #[Route('/home', name: 'app_home')]
+//     public function index(): Response
+//     {
+//         return $this->render('home/index.html.twig', [
+//             'controller_name' => 'HomeController',
+//         ]);
+//     }
+// }
+class HomeController {
+
+    function index (Request $request): Response {
+        // dd($request);
+        return new Response('Bonjour les gens' . $request->query->get('name', ' Inconnu'));
+    }
+}
